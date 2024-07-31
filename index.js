2894179599bfa4d1f5f93434faebc8fc3b55025a
alisalehiitp <alisalehiitp@gmail.com>
2024-07-31 17:44:42 +0000

    basic solution

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function disemvowel(str) {
-  return [...str].filter(el => !(['a', 'e', 'i', 'o', 'u']).includes(el.toLowerCase())).join('');
+function duplicateCount(text){
+  let textArr = [...text.toLowerCase()];
+  let dict = {}
+  let dupCount = 0
+
+  textArr.forEach(el => {
+    Object.keys(dict).includes(el) ? dict[el] += 1 : dict[el] = 1
+  })
+  
+  Object.values(dict).forEach(el => {
+    if (el > 1) dupCount += 1
+  })
+
+  return dupCount
 }
 
-console.log(disemvowel("This website is for losers LOL!"));
+console.log(duplicateCount("aabbcde"));
